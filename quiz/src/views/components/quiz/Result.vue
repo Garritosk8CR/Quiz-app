2b9b664b2542f292a5c96e2af1900c0a066045f8
Emilio Garro Rangel <emiliogarrorangelxboxone@hotmail.com>
2019-04-25 22:37:02 -0600

    added some style to result

--- quiz/src/views/components/quiz/Result.vue
+++ quiz/src/views/components/quiz/Result.vue
@@ -1,18 +1,21 @@
 <template>
    <v-container fluid fill-height>
       <v-layout align-center justify-center row fill-height>
-         <v-card max-width="344" class="mx-auto">
-            <v-card-title>Quiz results</v-card-title>
+         <v-card  max-width="344" class="mx-auto">
+            <v-card-title primary-title>
+                  <p class="display-1 mb-0">Quiz results</p>
+            </v-card-title>
             <v-card-text>
+               <v-divider></v-divider>
                <div>
-                  <p>Correct Answers: {{ totalCorrect }}</p>
+                  <p class="title font-weight-light">Correct Answers: {{ totalCorrect }}</p>
                </div>
                <div>
-                  <p>Incorrect Answers: {{ totalIncorrect }}</p>
+                  <p class="title font-weight-light">Incorrect Answers: {{ totalIncorrect }}</p>
                </div>
             </v-card-text>
             <v-card-actions>
-               <v-btn text @click="clickRestart()">Restart</v-btn>
+               <v-btn flat text color="secondary" @click="clickRestart()">Restart</v-btn>
             </v-card-actions>
          </v-card>
       </v-layout>
